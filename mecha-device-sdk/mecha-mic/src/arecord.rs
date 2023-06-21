3df2b326d6319830f65df0985e0438ee422e051b
dhruveshb-mecha <dhruveshb@mechasystems.com>
2023-06-21 11:58:02 +0530

    audio test added

--- mecha-device-sdk/mecha-mic/src/arecord.rs
+++ mecha-device-sdk/mecha-mic/src/arecord.rs
@@ -8,14 +8,21 @@ pub trait AudioInterface {
 }
 
 impl AudioInterface for MechaAudio {
-    fn record_audio(seconds: u32, filename: &str) {
+    fn record_audio(seconds: u32, filename: &str) -> () {
         let command = format!("arecord -Dplughw:0,0 -f dat -d {} {}", seconds, filename);
-        let mut process = Command::new(command).spawn().unwrap();
-        process.wait().unwrap();
+        Command::new("sh")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("failed to execute process");
     }
-    fn play_audio(filename: &str) {
-        let command = format!("aplay -Dplughw:0,0 {}", filename);
-        let mut process = Command::new(command).spawn().unwrap();
-        process.wait().unwrap();
+
+    fn play_audio(filename: &str) -> () {
+        let command = format!("aplay {}", filename);
+        Command::new("sh")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("failed to execute process");
     }
 }
