38df9c11e4ca03e5152cd4a2b5b7b69cd1340fba
dhruveshb-mecha <dhruveshb@mechasystems.com>
2023-06-30 15:45:48 +0530

    mecha power test current added

--- mecha-device-sdk/mecha-power-supply/src/power.rs
+++ mecha-device-sdk/mecha-power-supply/src/power.rs
@@ -1,5 +1,6 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader, Read};
+use std::num::ParseIntError;
 
 #[derive(Debug)]
 pub struct PowerSupply {
@@ -23,7 +24,7 @@ pub trait PowerSupplyInfo {
     fn info(&self) -> String;
     fn set_device(&mut self, device: &str);
     fn get_device(&self) -> &str;
-    fn get_current(&self) -> Result<u32, io::Error>;
+    fn get_current(&self) -> Result<i64, std::io::Error>;
 }
 
 pub struct Battery {
@@ -45,11 +46,18 @@ impl PowerSupplyInfo for Battery {
     }
 
     //to get current_now value read file from current_now path
-    fn get_current(&self) -> Result<u32, io::Error> {
-        let file = File::open(&self.currnet_now)?;
-        let reader = BufReader::new(file);
-        let line = reader.lines().next().unwrap()?;
-        let value = line.trim().parse::<u32>().unwrap();
+    fn get_current(&self) -> Result<i64, std::io::Error> {
+        let mut file = fs::File::open(&self.currnet_now)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        print!("file name {:?}", &file);
+        print!("{}", contents);
+
+        let value: i64 = contents
+            .trim()
+            .parse()
+            .map_err(|e: ParseIntError| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+
         Ok(value)
     }
 }
