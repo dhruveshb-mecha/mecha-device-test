7b7b114fb612b43e623d9f3fc8f2f027cfe63c5e
dhruveshb-mecha <dhruveshb@mechasystems.com>
2023-06-30 16:27:50 +0530

    mecha power test 3 added

--- src/tests/powertests/power_test3.rs
+++ src/tests/powertests/power_test3.rs
@@ -1,3 +1,9 @@
+use std::{
+    process::{exit, Command},
+    thread,
+    time::Duration,
+};
+
 use mecha_display::{Display, DisplayInterface};
 
 use crate::test_base::{log_message, question_prompt, Device, MessageType, TestAssertion};
@@ -5,6 +11,41 @@ use crate::test_base::{log_message, question_prompt, Device, MessageType, TestAs
 pub struct PowerTest3 {
     pub display_path: String,
     pub camera_path: String,
+}
+
+fn execute_gstreamer_command() {
+    let command = Command::new("gst-launch-1.0")
+        .arg("v4l2src")
+        .arg("device=/dev/video0")
+        .arg("!")
+        .arg("video/x-raw,width=1280,height=720,framerate=30/1")
+        .arg("!")
+        .arg("vpuenc_h264")
+        .arg("!")
+        .arg("h264parse")
+        .arg("!")
+        .arg("vpudec")
+        .arg("!")
+        .arg("imxvideoconvert_g2d")
+        .arg("!")
+        .arg("queue")
+        .arg("!")
+        .arg("waylandsink")
+        .spawn();
+
+    match command {
+        Ok(mut child) => {
+            // Wait for 15 seconds
+            thread::sleep(Duration::from_secs(15));
+
+            // Terminate the GStreamer command
+            child.kill().expect("Failed to kill the GStreamer command.");
+        }
+        Err(e) => {
+            eprintln!("Failed to execute GStreamer command: {}", e);
+            exit(1);
+        }
+    }
 }
 
 impl TestAssertion for PowerTest3 {
@@ -52,6 +93,15 @@ impl TestAssertion for PowerTest3 {
             MessageType::Action,
             "Waiting for 15 seconds to check power consumption",
         );
+
+        //log message that we're previewing the camera
+        log_message(
+            Device::Power,
+            MessageType::Action,
+            "Previewing the camera for 15 seconds",
+        );
+
+        execute_gstreamer_command();
 
         Ok(true)
     }
